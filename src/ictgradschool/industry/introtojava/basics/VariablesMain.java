c9a987aaf22f78746e1913c78e27c54827c0c25f
Thanusiya Vijayarajan <thanusiyavijayarajan@gmail.com>
2026-03-06 11:40:48 +1300

    completed 1.basic -> VariablesMain

--- src/ictgradschool/industry/introtojava/basics/VariablesMain.java
+++ src/ictgradschool/industry/introtojava/basics/VariablesMain.java
@@ -17,17 +17,17 @@ public class VariablesMain {
         // TODO: When you first open this class, try running it and check to see that you can see the output in the console
 
         // TODO Step 1) add an age after the '=' operator to assign a value to the variable then run the code to test it
-        int age = 0;
+        int age = 1;
         // The System.out... statement outputs the age combined with the String literal "The age is "
-        System.out.println("The age is " + age);
+        System.out.println("The age is :" + age);
 
         // TODO Step 2) Create a second int variable called monthsSinceBirthday and assign a number of months between 1-11
-        int monthsSinceBirthday = 1;
+        int monthsSinceBirthday = 7;
         // TODO Step 3) Create a 'System.out...' statement to output the monthsSinceBirthday variable with an appropriate message
-        System.out.println("Months since Birthday "+ monthsSinceBirthday);
+        System.out.println("Months since Birthday :"+ monthsSinceBirthday);
         // TODO  Step 4) Follow the instructions in the lab to calculate the value of the 'daysOnEarth' variable
-        int daysOnEarth = monthsSinceBirthday *30;
-        System.out.println("Days on earth: " + daysOnEarth);
+        int daysOnEarth = (age * 365) +monthsSinceBirthday *30;
+        System.out.println("Days on earth: "  + daysOnEarth);
 
 
     }
